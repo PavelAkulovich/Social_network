b00be7a4d779d533f4923e1bdcab8aa73842ed63
PavelAkulovich <pasha860@mail.ru>
2022-07-18 14:22:21 +0300

    add: create Dialogs component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Content from './components/Content/Content.jsx';
+import Dialogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className='wrapper'>
       <Header />
       <Navbar />
-      <Content />
+      <Dialogs />
+      {/* <Content /> */}
     </div>
 
   );
